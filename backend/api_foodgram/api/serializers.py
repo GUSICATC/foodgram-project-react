7f35fd9e30857ad0b8c2df4ff25ef25de4cc461f
GUSIC <Gusicc@yandex.ru>
2023-03-17 16:53:42 +0300

    1

--- backend/api_foodgram/api/serializers.py
+++ backend/api_foodgram/api/serializers.py
@@ -16,21 +16,20 @@ class Hex2NameColor(serializers.Field):
         return value
 
     def to_internal_value(self, data):
-        try:
-            data = webcolors.hex_to_name(data)
-        except ValueError:
-            raise serializers.ValidationError("Для этого цвета нет имени")
+        # try:
+        #     data = webcolors.hex_to_name(data)
+        # except ValueError:
+        #     raise serializers.ValidationError("Для этого цвета нет имени")
         return data
 
 
 class Base64ImageField(serializers.ImageField):
-    pass
-    # def to_internal_value(self, data):
-    #     if isinstance(data, str) and data.startswith("data:image"):
-    #         format, imgstr = data.split(";base64,")
-    #         ext = format.split("/")[-1]
-    #         data = ContentFile(base64.b64decode(imgstr), name="temp." + ext)
-    #     return super().to_internal_value(data)
+    def to_internal_value(self, data):
+        if isinstance(data, str) and data.startswith("data:image"):
+            format, imgstr = data.split(";base64,")
+            ext = format.split("/")[-1]
+            data = ContentFile(base64.b64decode(imgstr), name="temp." + ext)
+        return super().to_internal_value(data)
 
 
 class UserSerializer(serializers.ModelSerializer):
