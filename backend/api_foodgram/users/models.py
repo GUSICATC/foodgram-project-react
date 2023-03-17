18af6548164011fd6abf5815e6e11b250df3d4e3
GUSIC <Gusicc@yandex.ru>
2023-03-17 16:21:08 +0300

    1

--- backend/api_foodgram/users/models.py
+++ backend/api_foodgram/users/models.py
@@ -14,7 +14,9 @@ class User(AbstractUser):
         unique=True,
         verbose_name="Емаил",
     )
-    first_name = models.CharField(max_length=150, verbose_name="Имя", blank=True, null=True)
+    first_name = models.CharField(
+        max_length=150, verbose_name="Имя", blank=True, null=True
+    )
     last_name = models.CharField(
         max_length=150,
         verbose_name="Фамилия",
